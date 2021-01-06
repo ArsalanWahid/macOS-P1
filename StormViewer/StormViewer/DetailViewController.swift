0359c543ceb49410c4716f7f3f9cb81f6b2dab40
arsalanwahid <arslanwahid1993@gmail.com>
2021-01-06 17:46:22 +0500

    added content and controllers for UI

---
+++ StormViewer/StormViewer/DetailViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.swift
+//  StormViewer
+//
+//  Created by Arsalan Wahid Asghar on 06/01/2021.
+//
+
+import Cocoa
+
+class DetailViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
---
+++ StormViewer/StormViewer/SourceViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SourceViewController.swift
+//  StormViewer
+//
+//  Created by Arsalan Wahid Asghar on 06/01/2021.
+//
+
+import Cocoa
+
+class SourceViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}

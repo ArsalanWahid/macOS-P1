6cad0d8615c45a4f8ca21b4d3d821e75c4251cf8
arsalanwahid <arslanwahid1993@gmail.com>
2021-01-06 20:32:39 +0500

    now showing file names in split view

--- StormViewer/StormViewer/SourceViewController.swift
+++ StormViewer/StormViewer/SourceViewController.swift
@@ -7,11 +7,33 @@
 
 import Cocoa
 
-class SourceViewController: NSViewController {
+class SourceViewController: NSViewController, NSTableViewDelegate, NSTableViewDataSource {
 
+    var pictures = [String]()
+    @IBOutlet var tableView: NSTableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do view setup here.
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("nssl") {
+                pictures.append(item)
+            }
+        }
+        
     }
     
+    //MARK:- TableView
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        return pictures.count
+    }
+    
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        guard let vw = tableView.makeView(withIdentifier: tableColumn!.identifier, owner: self) as? NSTableCellView else {return nil}
+        
+        vw.textField?.stringValue = pictures[row]
+        return vw
+    }
 }
